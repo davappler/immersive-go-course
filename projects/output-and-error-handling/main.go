34538439025fe381f9507dd85638c6433ea2c019
davibaweja <davibaweja@gmail.com>
2024-05-29 23:35:18 +0100

    adding the solution for first project error handling and output

---
+++ projects/output-and-error-handling/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func main() {
+	c := http.Client{Timeout: time.Duration(1) * time.Second}
+
+	url := "http://localhost:8080"
+	max_tries := 5
+
+	fmt.Println("----------------------------")
+
+	for i := 0; i < max_tries; i++ {
+		resp, err := c.Get(url)
+		if err != nil {
+			fmt.Println("Error from request", err)
+			return
+		}
+
+		if resp.StatusCode == http.StatusTooManyRequests {
+			retryAfter := resp.Header.Get("Retry-After")
+			retryAfterInteger, err := strconv.Atoi(retryAfter)
+			if err != nil {
+				fmt.Println("Error parsing string")
+				return
+			}
+
+			if retryAfterInteger > 1 {
+				fmt.Println("There is a little delay...")
+			}
+
+			if retryAfterInteger > 5 {
+				fmt.Println("Server is overloaded we can not get the weather details sorry")
+				return
+			}
+
+			duration := time.Duration(retryAfterInteger) * time.Second
+			time.Sleep(duration)
+		} else {
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Println("error reading response body")
+				return
+			}
+
+			fmt.Println(string(body))
+			return
+		}
+
+	}
+
+	fmt.Println("Max tries reached! Exiting")
+}
